e9d5926463869631cd8e740b27e56173a314c639
Jens <jens.w.stockmann@gmail.com>
2023-12-05 08:51:30 +0200

    db config

--- shopping-lists/deps.js
+++ shopping-lists/deps.js
@@ -1,3 +1,4 @@
 export { serve } from "https://deno.land/std@0.202.0/http/server.ts";
 export { configure } from "https://deno.land/x/eta@v2.2.0/mod.ts";
 export { renderFile } from "https://deno.land/x/eta@v2.2.0/mod.ts";
+export { postgres } from "https://deno.land/x/postgresjs@v3.4.2/mod.js";
